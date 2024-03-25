ea7f029853b36d61d48f8fd434a01a992a22ef85
Mqleet <mqleet@gmail.com>
2024-03-25 16:28:40 +0800

    baseline搭好了

--- utils/result_visual.py
+++ utils/result_visual.py
@@ -2,14 +2,14 @@ import sys
 sys.path.append("/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/phynet") 
 from library import result_visual,mkdir
 if __name__=='__main__':
-    num=0
-    name = f'sam_ref_dropout_full_0.03'#一、生成的文件存放路径，最好与二里一致1536_1536_sam_pi_01_prop_pi/2024-02-21-21-53
+    num=12000
+    name = f'baseline'#一、生成的文件存放路径，最好与二里一致1536_1536_sam_pi_01_prop_pi/2024-02-21-21-53
     
     # 二、读取的ref或Sam路径
-    ref_path = f'/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/result/net_model_Dropout_full/net_model_Dropout_full_all_p/1536_1536_ref_pi_01_prop_pi/2024-03-06-20-54/img_txt_folder/0.03/27000_pred.txt'
-    sam_path=  f'/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/result/net_model_Dropout_full/net_model_Dropout_full_all_p/1536_1536_sam_pi_01_prop_pi/2024-03-07-21-40/img_txt_folder/0.03/27000_pred.txt'
-    gt_ref_path = '/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/traindata/gt/1536_1536_ref_pi_01_prop_pi.txt'
-    gt_sam_path = '/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/traindata/gt/1536_1536_sam_pi_01_prop_pi.txt'
+    ref_path = f'/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/result/net_model_Dropout_full/2024-03-19-12-16/img_txt_folder/colon1_phase_ref_prop001/12000_PredPha.txt'
+    sam_path=  f'/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/result/net_model_Dropout_full/2024-03-19-12-16/img_txt_folder/colon1_phase_sam_prop001/12000_PredPha.txt'
+    gt_ref_path = '/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/traindata/pi/pha_cropped/colon1/phase_ref_prop001.txt'
+    gt_sam_path = '/mnt/data/optimal/tangyuhang/workspace/iopen/ai4optical/phynet_git/traindata/pi/pha_cropped/colon1/phase_sam_prop001.txt'
 
     # ref = my_readtxt(sam_path)
     # sam = my_readtxt(gt_sam_path)   
